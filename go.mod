4f292d6279717b79490b545375252c0adc71ac7e
skyleaworlder <870033938@qq.com>
2021-04-06 19:17:59 +0800

    M: make Default Constructor DEFAULT again: remove parameter logPath(string) in NewDefaultLoadBalancer, and mv logger config logic to LdblserMapStuffer

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/skyleaworlder/ngoinx
 go 1.15
 
 require (
+	github.com/sirupsen/logrus v1.8.1
 	github.com/tidwall/gjson v1.7.4
 	github.com/umpc/go-sortedmap v0.0.0-20180422175548-64ab94c482f4
 )
--- src/utils/Logger.go
+++ src/utils/Logger.go
@@ -6,6 +6,17 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+// LoggerConfig is a struct to contain logrus's config
+type LoggerConfig struct {
+	LogPath     string
+	LogFileName string
+	LogSuffix   string
+
+	LogFormatter log.Formatter
+	LogOutput    io.Writer
+	LogLevel     log.Level
+}
+
 // LoggerGenerator is a tool function to generate *log.Entry
 func LoggerGenerator(formatter log.Formatter, output io.Writer, level log.Level) *log.Entry {
 	entry := log.NewEntry(&log.Logger{})
