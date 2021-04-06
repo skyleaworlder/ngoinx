7311e0647fc6cb0fb0fbb172bf02f451037f6e52
skyleaworlder <870033938@qq.com>
2021-04-06 18:53:37 +0800

    M: add log path in template.json

--- src/config/Service.go
+++ src/config/Service.go
@@ -5,12 +5,14 @@ import "github.com/tidwall/gjson"
 // Service is a struct (unmarshal from config.json)
 type Service struct {
 	Listen  uint16
+	Log     string
 	Proxies []Proxy
 }
 
 // Unmarshal is to implement method in interface "Unmarshaler"
 func (s *Service) Unmarshal(res gjson.Result) {
 	s.Listen = uint16(res.Get("listen").Uint())
+	s.Log = res.Get("log").String()
 	for _, proxy := range res.Get("proxy").Array() {
 		p := Proxy{}
 		p.Unmarshal(proxy)
