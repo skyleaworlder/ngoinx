61703899a6f2bd5f167ec996b09805add8591707
skyleaworlder <870033938@qq.com>
2021-04-06 18:22:28 +0800

    M: add logrus in Server.go

--- src/server/Server.go
+++ src/server/Server.go
@@ -1,23 +1,52 @@
 package server
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"io/ioutil"
-	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
 	"time"
 
+	log "github.com/sirupsen/logrus"
 	"github.com/skyleaworlder/ngoinx/src/config"
 	"github.com/skyleaworlder/ngoinx/src/ldbls"
+	"github.com/skyleaworlder/ngoinx/src/utils"
 )
 
+// Server is a struct
+type Server struct {
+	serverLogger *log.Entry
+	svcLoggers   []*log.Entry
+}
+
+// NewNgoinxServer is a constructor
+// service is always config.Svc
+func NewNgoinxServer(logger *log.Entry, service []config.Service) (s *Server) {
+	// generate svcLoggers
+	// loggers are used in handlerGenerator, in order to get logs when server serving
+	svcLoggers := []*log.Entry{}
+	for idx, svc := range service {
+		logName := "Ngoinx-" + strconv.Itoa(idx) + "-Port-" + strconv.Itoa(int(svc.Listen)) + ".log"
+		fd, err := os.OpenFile(logName, os.O_CREATE|os.O_WRONLY, 0755)
+		if err != nil {
+			fmt.Println("ngoinx.server.NewNgoinxServer error:", err.Error())
+			return nil
+		}
+		svcLogger := utils.LoggerGenerator(&log.TextFormatter{}, fd, log.DebugLevel)
+		svcLoggers = append(svcLoggers, svcLogger)
+	}
+	return &Server{serverLogger: logger, svcLoggers: svcLoggers}
+}
+
 // Serve export for test
-func Serve() {
-	for _, svc := range config.Svc {
-		handler := handlerGenerator()
+func (s *Server) Serve() (err error) {
+	s.serverLogger.WithField("status", "INIT_BEGIN").Info("Server begins initializing")
+	for idx, svc := range config.Svc {
+		handler := handlerGenerator(s.svcLoggers[idx])
 		s := &http.Server{
 			Addr:           ":" + strconv.Itoa(int(svc.Listen)),
 			Handler:        handler,
@@ -27,34 +56,39 @@ func Serve() {
 		}
 		go s.ListenAndServe()
 	}
-	fmt.Println("serve finish init.")
+	s.serverLogger.WithField("status", "INIT_END").Info("Server finishs initializing, and prepare for serving")
 	time.Sleep(1000 * time.Second)
+	return errors.New("hahaha")
 }
 
-func handlerGenerator() (handler http.HandlerFunc) {
+func handlerGenerator(logger *log.Entry) (handler http.HandlerFunc) {
 	return func(rw http.ResponseWriter, r *http.Request) {
 		body, err := ioutil.ReadAll(r.Body)
 		if err != nil {
-			log.Println("ngoinx.server.Server.handlerGenerator error: ioutil.ReadAll failed :", err.Error())
+			logger.Warningln("ngoinx.server.Server.handlerGenerator error: ioutil.ReadAll failed :", err.Error())
 			return
 		}
 		// get scheme://userinfo@host
-		fmt.Println("url.path:", r.URL.Path)
-		// fmt.Println("map get:", ldbls.LdblserMap, ldbls.LdblserMap[r.URL.Path])
 		addr, err := ldbls.LdblserMap[r.URL.Path].GetAddr(r)
-		fmt.Println("addr chosen:", addr)
 		if err != nil {
-			log.Println("ngoinx.server.Server.handlerGenerator error: ldbls.GetAddr failed :", err.Error())
+			logger.Warningln("ngoinx.server.Server.handlerGenerator error: ldbls.GetAddr failed :", err.Error())
 			return
 		}
 
 		// cat relayURL
 		relayURL := addr + r.URL.Path + r.URL.RawQuery + r.URL.Fragment
-		fmt.Println("relay URL:", relayURL)
+
+		logger.WithFields(log.Fields{
+			"url.path":    r.URL.Path,
+			"addr chosen": addr,
+			"relay URL":   relayURL,
+			"map get":     ldbls.LdblserMap[r.URL.Path],
+		}).Info("This info appear only if a request comes")
+
 		req, err := http.NewRequest(r.Method, relayURL, strings.NewReader(string(body)))
-		fmt.Println(req.Header)
+		logger.Info("request's Header:", req.Header)
 		if err != nil {
-			log.Println("ngoinx.server.Server.handlerGenerator error: http.NewRequest failed :", err.Error())
+			logger.Warningln("ngoinx.server.Server.handlerGenerator error: http.NewRequest failed :", err.Error())
 			return
 		}
 		// copy r's Header to req
@@ -62,7 +96,7 @@ func handlerGenerator() (handler http.HandlerFunc) {
 
 		resp, err := http.DefaultClient.Do(req)
 		if err != nil {
-			log.Println("ngoinx.server.Server.handlerGenerator error: http.DefaultClient.Do failed :", err.Error())
+			logger.Warningln("ngoinx.server.Server.handlerGenerator error: http.DefaultClient.Do failed :", err.Error())
 			return
 		}
 		defer resp.Body.Close()
--- src/test/ldbls_test.go
+++ src/test/ldbls_test.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
+	"github.com/sirupsen/logrus"
 	"github.com/skyleaworlder/ngoinx/src/ldbls"
 	"github.com/skyleaworlder/ngoinx/src/server"
 	"github.com/skyleaworlder/ngoinx/src/utils"
@@ -14,6 +16,11 @@ func Test_LdblsMapStuffer(t *testing.T) {
 	ldbls.LdblserMapStuffer(svc)
 	fmt.Println(ldbls.LdblserMap)
 
-	server.Serve()
+	fd, _ := os.OpenFile("Ngoinx-Server.log", os.O_CREATE|os.O_WRONLY, 0755)
+	logger := utils.LoggerGenerator(&logrus.TextFormatter{}, fd, logrus.DebugLevel)
+	s := server.NewNgoinxServer(logger, svc)
 
+	if err := s.Serve(); err != nil {
+		fmt.Println("wuhu! end!")
+	}
 }
