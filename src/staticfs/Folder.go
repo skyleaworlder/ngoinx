ed0f97f02b8acbc044e5d7c8aea8a7d2194f15a9
skyleaworlder <870033938@qq.com>
2021-04-08 20:34:16 +0800

    M: add feature about deleting static resource cache

--- src/staticfs/Folder.go
+++ src/staticfs/Folder.go
@@ -1,10 +1,12 @@
 package staticfs
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
 )
 
 // Folder is a struct implement http.FileSystem
@@ -36,8 +38,33 @@ func (sf *Folder) Open(name string) (file http.File, err error) {
 	return
 }
 
+// Create is a method used to create file in static folder (managed by Folder struct)
+func (sf *Folder) Create(name string) (file *os.File, err error) {
+	fileName := filepath.Join(sf.path, filepath.FromSlash(filepath.Clean(name)))
+	file, err = os.Create(fileName)
+	return
+}
+
 // mkdir wrap os.MkdirAll function
 // mode default 0755
 func (sf *Folder) mkdir() (err error) {
 	return os.MkdirAll(sf.path, 0755)
+}
+
+// clean is to delete some files that are not to standard
+func (sf *Folder) clean() {
+	filepath.Walk(sf.path, func(path string, info os.FileInfo, err error) error {
+		isDir := info.IsDir()
+		bodyNull := (info.Size() == 0)
+		outDated := (time.Now().Sub(info.ModTime()) >= time.Minute)
+		if isDelete := !isDir && (bodyNull || outDated); isDelete {
+			err = os.Remove(path)
+			if err != nil {
+				fmt.Println("remove file failed:", info.Name())
+			} else {
+				fmt.Println("remove file:", info.Name())
+			}
+		}
+		return err
+	})
 }
--- src/staticfs/FolderManager.go
+++ src/staticfs/FolderManager.go
@@ -1,7 +1,9 @@
 package staticfs
 
 import (
+	"errors"
 	"fmt"
+	"time"
 
 	"github.com/skyleaworlder/ngoinx/src/config"
 	"github.com/skyleaworlder/ngoinx/src/utils"
@@ -30,18 +32,34 @@ func (sfm *FolderManager) Init(Service []config.Service) (err error) {
 	return
 }
 
+// Serve is to raise clean
+func (sfm *FolderManager) Serve() (err error) {
+	return sfm.clean()
+}
+
 // StripURLPathPrefix is a tool function
 // path: r.URL.Path
 // return "" only when (r.URL.Path is src) or fail to find prefix in Folders
 // return ok(false) when failing
-func (sfm *FolderManager) StripURLPathPrefix(path string) (srcPath SrcPath, result string, ok bool) {
+func (sfm *FolderManager) StripURLPathPrefix(path string) (srcPath SrcPath, dstPath string, ok bool) {
 	for srcPath := range sfm.Folders {
 		// Folders' keys are all from service[i].proxy[j].src
 		// check whether path has the prefix "service[i].proxy[j].src"
 		fmt.Println("fordebug, path(r.URL.Path):", path)
-		if result, ok := utils.StripURLPathPrefix(path, string(srcPath)); ok {
-			return srcPath, result, ok
+		if dstPath, ok := utils.StripURLPathPrefix(path, string(srcPath)); ok {
+			return srcPath, dstPath, ok
 		}
 	}
 	return "", "", false
+}
+
+func (sfm *FolderManager) clean() (err error) {
+	for running := true; running; {
+		fmt.Println("clean!!!!!")
+		for _, folder := range sfm.Folders {
+			folder.clean()
+		}
+		time.Sleep(time.Second * 10)
+	}
+	return errors.New("ngoinx.staticfs.FolderManager.clean quit")
 }
